bcae3bf310237738d739c7a3b9898fb7cd4702ad
Karam Abbas <kma69362@gmail.com>
2025-08-18 13:43:57 -0700

    commit

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 memes = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRBhEJ_V7oOIsyFS9S-wLni5qDafazxKDVJA&s',
-    'https://i1.sndcdn.com/artworks-JJOu6dvb4Swg3f5K-bl6o5g-t500x500.jpg',
+    'https://external-preview.redd.it/steven-here-v0-xmpw7iV6gdrJ7OjKeH75kMPD2YoGigV3PJKqBSMFD3g.png?format=pjpg&auto=webp&s=fd88aa82b11a098afd8bc4c16cecf7326c06f772',
     'https://cdn-images.dzcdn.net/images/cover/173ba7fc2bbf9e902b61a5636a3b6dc7/0x1900-000000-80-0-0.jpg',
     'https://media1.tenor.com/m/2hO1qFdswTcAAAAd/adrian-adrian-explain-our-friend-group.gif',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN1SZrkCViLLUFVSK0dsxEEirzrMYWGlwIUUn9rK_Mw9chUXgGjN564m2uGkTkJdNB44g&usqp=CAU',
     'https://i.redd.it/mcrnke8jc50e1.jpeg',
     'https://preview.redd.it/67m-rate-my-looks-v0-zwvnlpu0ayhf1.jpeg?width=640&crop=smart&auto=webp&s=3e3517c372fcbbab5072d3f082ebdc55c053f708',
-    'https://preview.redd.it/why-is-this-in-my-recommended-v0-9t6or3qp67fe1.jpeg?width=640&crop=smart&auto=webp&s=d97421cc7e484881f9ece8d684069fb04997e1c2',
+    'https://preview.redd.it/alan-moore-says-your-not-sigma-v0-gt4wjlytp6ud1.jpeg?auto=webp&s=a71a78c6309cc1bbe76cc4294257d3c7b72bad36',
     'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSwtlYZuMSBALPJIR5RblVT2UL2D5SrLuxU52ppsBkmoWgjHQ1L',
     'https://content.imageresizer.com/images/memes/Sigma-Cat-meme-7p2ecu.jpg',
     'https://imageproxy.ifunny.co/resize:640x,quality:90x75/images/138918d716ce66c110365b9c6248b20e45959dda4109459fbc195e8fde9f52a0_3.jpg',
@@ -15,22 +15,22 @@ memes = [
     'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1raLyw.img?w=690&h=388&m=6',
     'https://preview.redd.it/the-kendrick-mustard-of-duty-meme-is-huge-right-now-v0-evovdf5yrnve1.jpeg?auto=webp&s=0428c2e728bf7d41cd67829ed996b8e7918a99e8',
     'https://i.ytimg.com/vi/bIsp1K8eJG0/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDUw_GHc5K2xp752g2bBCVbI8eZ9g',
-    'https://preview.redd.it/on-this-day-three-years-ago-u-lavameteor-posted-they-sus-v0-zr8cfaqja50c1.jpg?width=640&crop=smart&auto=webp&s=79340572b4618438ee98d910ecaa996812872e76',
+    'https://media1.tenor.com/m/88VE5S-BscUAAAAC/i-just-a-baby-baby.gif',
     'https://media1.tenor.com/m/4eALS2xDONAAAAAd/oi-oi-oi-baka.gif',
     'https://i.ytimg.com/vi/NhHb9usKy6Q/maxresdefault.jpg',
-    'https://media1.tenor.com/m/2ndcvKu-928AAAAd/bring-out-the-fine-china.gif',
+    'https://preview.redd.it/i-have-a-question-v0-kj2ox7h8f5wd1.jpeg?auto=webp&s=77e375b1c83b4abcf5ded4f2dc579abc7759de7d',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8qUan4GWRBJyJtlKPr5lDEwLBZw37tQ2rKQ&s',
 ]
 
 quotes = [
     'Shocked Black Guy',
-    'Evil rick AI',
+    "Steven here... I'm looking for Fifi.",
     'Tuff Trollface 💀', // Dos Uno (those who know) 💀
     'Adrian, explain our friend group.',
     "Coffee spelt backwards is eeffoc. Which is crazy.",
     'Kid insults Dhar Mann, gets dragged to hell by Satan himself!',
     'HEEEEEY SIX SEVENNN!', // ts (this) is hella tuff boiiiii
-    'Minecraft but I have...',
+    "Sorry... You're NOT a Sigma.",
     'Put me in a lamar jackson edit.', // People did hilariously terrible edits of this guy
     'Monday left me broken.',
     "It comes with eggroll.",
@@ -39,10 +39,10 @@ quotes = [
     'Eye of Rah.',
     'Call of Mustard meme is going viral',
     "Does he know?",
-    'When the imposter is sus.',
+    "I'm just a bebeh!",
     'This dude is not real.',
     'You are my sunshine, my only sunshine.',
-    'Honey we have guests Bring out the Fine China.',
+    "Bro thinks he's the thinker",
     '40+ year old PluggnB rapper 🔥🙏 did unc snap?', // Pluggnb is a hip-hop subgenre, just so you know. 
 ]
 
@@ -65,8 +65,12 @@ function renderGallery(){
 }
 renderGallery();
 
-function searchContent(input){
 
+function searchContent(){
+    memes_and_quotes = [...memes, ...quotes]
+    search = $("#search").val();
+    new RegExp(search, memes_and_quotes);
+    // use new RegExp()
 }
 
 function addContent(){
